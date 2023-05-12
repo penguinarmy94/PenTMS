1875866d4858da9ac145498edf34c4cd89a33cd5
Daniel <martinezd1045@gmail.com>
2023-05-12 13:15:45 -0700

    Added Hikes and HikeLibrary to the functions file. Trying to think of different functions to add to HikeLibrary. Also, changed all instances of 'trail' to 'hike'.

--- functions.py
+++ functions.py
@@ -1,7 +1,4 @@
 from typing import List, Any, Dict, Tuple, Union, Generator, Optional
-
-
-
 # creating a Class that the user can utilize in order to create different national parks
 
 STATES = ["CA", "FL", "TX", "NY", "RI", "WS"]
@@ -13,12 +10,71 @@ class Wildlife:
         self.species: str = species
 
 class Monument: # TODO
-    def __init__(self):
-        pass
-    
+    def __init__(self, name: str, year_built: int, historic_value: str):
+        self.year: int = year_built
+        self.name: str = name
+        self.historic_value: str = historic_value
+
+    def __repr__(self) -> str:
+        return f"The monument's name is {self.name}, it was built in {self.year}, and it is valuable because {self.historic_value}"
 class MonumentLibrary: # TODO
     def __init__(self):
+        self.__library: List[Monument] = []
+    
+    def add_monument(self, monument_name: str):
+        self.__library.append(monument_name)
+    
+    def remove_monument(self, monument_name: str):
+        index = 0
+        while index < len(self.__library):
+            monument: Monument = self.__library[index]
+            if monument.name == monument_name:
+                self.__library.remove(monument)
+                break
+            else:
+                index += 1
+
+    def __contains__(self, name: str) -> bool:
+        for monument in self.__library:
+            if monument.name == name:
+                return True
+            
+        return False
+
+    def __len__(self) -> int:
+        return len(self.__library)
+    
+    def __iter__(self) -> Generator[Monument, None, None]:
+        for monument in self.__library:
+            yield monument
+
+    def __repr__(self) -> str:
+        return "\n".join([str(monuments) for monuments in self.__library])
+    
+
+class Hikes:
+    def __init__(self, name: str, length: float, difficulty: str, elevation_gain: int, type_of_trail: str): 
+        self.length: float = length
+        self.elevation_gain: int = elevation_gain
+        self.trail_type: str = type_of_trail
+        self.difficulty: str = difficulty
+        self.name: str = name
+
+    def __repr__(self) -> str:
+        return f"The trail name is {self.name}, it is a {self.trail_type} that is {self.length} miles long with an elevation gain of {self.elevation_gain} feet. This trail is for more {self.difficulty} hikers, hike at your own risk."
+    
+class HikeLibrary: 
+    def __init__(self): 
+        self.__library = List[Hikes] = []
+    
+    def add_hikes(self, hike_name: str):
+        self.__library.append(hike_name)
+    
+    def remove_hikes(self, hike_name: str):
+        index = 0
         pass
+
+
 
 class NationalPark:
     def __init__(self, name: str, county: str, state: str, terrain_type: str, perimeter: float):
@@ -48,7 +104,7 @@ class NationalPark:
     
     @property
     def monuments(self) -> Tuple[Monument]:
-        pass # TODO
+        return tuple(self.__monuments)
         
     
     @county.setter
@@ -65,13 +121,19 @@ class NationalPark:
         else:
             self.__state = new_state
     
-    def add_trail(self, new_trail: str):
-        self.__trails.append(new_trail)
+    def add_hikes(self, name: str, length: float, difficulty: str, elevation_gain: int, trail_type):
+        new_hike: Hikes = Hikes(name= name, length= length, difficulty= difficulty, elevation_gain= elevation_gain, type_of_trail=trail_type)
+        self.__trails.append(new_hike)
     
-    def add_monument(self, monument: str):
-        self.__monuments.append(monument)
+    def add_monument(self, name: str, **opt):
+        monuments: Monument = Monument(name= name, year_built= opt.get("year_built", 1920), historic_value= opt.get("historic_value", "unknown"))
+        self.__monuments.add_monument(monuments)
     
-    def trail_count(self) -> int:
+    def remove_monument(self, monument_name):
+        if monument_name in self.__monuments:
+            self.__monuments.remove_monument(monument_name) 
+
+    def hikes_count(self) -> int:
         return len(self.__trails)
     
     def monument_count(self) -> int:
@@ -106,20 +168,51 @@ class NationalParkLibrary:
             yield park
 
 if __name__ == "__main__":
-    a = NationalPark(name="Yosemite", county="Santa Clara", state="CA", terrain_type="desert", perimeter=5000)
-    b = NationalPark(name="Yellowstone", county="Pensicola", state="NY", terrain_type="forest", perimeter=9825.67)
-    lib = NationalParkLibrary()
-    
-    a.add_wildlife(species="deer", name="Dolly", number_of_legs=4)
-    a.add_wildlife(species="wolf", name="Speedy", number_of_legs=4)
-    a.add_wildlife(species="squirrel", name="Nutter", number_of_legs=2)
-    
-    b.add_wildlife(species="moose", name="Cranky", number_of_legs=4)
-    b.add_wildlife(species="parrot", name="Chatty", number_of_legs=2)
-    
-    lib.add_park(a)
-    lib.add_park(b)
-    
-    print(a,b)
-    print(a.monuments, b.monuments) # [Monument(..), Monument(...)] [Monument(..), Monument(..)]
+    # a = NationalPark(name="Yosemite", county="Santa Clara", state="CA", terrain_type="desert", perimeter=5000)
+    # b = NationalPark(name="Yellowstone", county="Pensicola", state="NY", terrain_type="forest", perimeter=9825.67)
+    # lib = NationalParkLibrary()
 
+    
+    # a.add_wildlife(species="deer", name="Dolly", number_of_legs=4)
+    # a.add_wildlife(species="wolf", name="Speedy", number_of_legs=4)
+    # a.add_wildlife(species="squirrel", name="Nutter", number_of_legs=2)
+    
+    # b.add_wildlife(species="moose", name="Cranky", number_of_legs=4)
+    # b.add_wildlife(species="parrot", name="Chatty", number_of_legs=2)
+
+    # a.add_monument(name="Big Tower", year_built= 1930, historic_value= "None")
+    # a.add_monument(name="Small Tower", year_built= 1850, historic_value="Some")
+
+    # b.add_monument(name="Big Rock", year_built= 1950, historic_value= "Giant Rock! ")
+
+    # a.add_monument()
+    
+    # lib.add_park(a)
+    # lib.add_park(b)
+    
+    # a = Monument(name="Big Tower", year_built= 1930, historic_value= "None")
+    # b = Monument(name="Small Tower", year_built= 1850, historic_value="Some")
+    
+    
+    parks = NationalPark("Yosemite", "Mariposa", "CA", "Mixed", 5000)
+    
+
+    # # assert parks.monument_count() == 2
+    # print(parks.monuments)
+    # parks.remove_monument(a.name)
+    # print(parks.monuments)
+
+    a = Hikes(name="Jones Peak",length= 7,difficulty= "Advanced",elevation_gain= 3000,type_of_trail= "Out and Back")
+    b = Hikes("Strawberry Peak", 6.5, "Advanced", 2700, "Out and Back")
+
+    parks.add_hikes(a.name, a.length, a.difficulty, a.elevation_gain, a.trail_type)
+    parks.add_hikes(b.name, b.length, b.difficulty, b.elevation_gain, b.trail_type)
+    print(a, b)
+
+    assert parks.hikes_count() == 2
+    
+    # print(a,b)
+    # print(a.monuments, b.monuments) # [Monument(..), Monument(...)] [Monument(..), Monument(..)]
+
+    #monumentlibrary object has no attribute append 
+    #monumentlibrary len() function
