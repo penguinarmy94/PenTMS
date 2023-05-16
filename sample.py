f3ce2714e60d63321c003bf7793947256def2bc7
Luis Otero <lotero@capellasystems.net>
2023-05-16 00:05:41 -0700

    Modified find_files function for proper type annotations and format

--- sample.py
+++ sample.py
@@ -3,14 +3,14 @@ from typing import Tuple, List
 
 tuple_of_extensions = (".html", ".cpp", ".xml", ",doc", "txt", "py")
 
-def find_files_with_extension(directory: str, depth: int = 0, extension: str = tuple_of_extensions) -> Tuple[str]:
+def find_files_with_extension(directory: str, depth: int = 0, extensions: Tuple[str] = tuple_of_extensions) -> Tuple[str]:
     """
         Recursively find files with a specific extension
         
         Args:
             directory (str): the directory to traverse
-            depth (int, optional): the level of subdirectories to traverse. Default is to not traverse subdirectories '0'
-            extension (str, optional): the extension to look for. Default is '.txt'.
+            depth (int, optional): the level of subdirectories to traverse. Default is to not traverse subdirectories '0'. Set to '-1' to traverse all subdirectories.
+            extensions (Tuple[str], optional): the extensions to look for.
         
         Return:
             Tuple[str]: the list of full paths to files within the directory that match the extension rule
@@ -19,10 +19,10 @@ def find_files_with_extension(directory: str, depth: int = 0, extension: str = t
     files: List[str] = []
     
     for file_object in os.scandir(directory):
-        if file_object.is_file() and file_object.name.endswith(extension):
+        if file_object.is_file() and file_object.name.endswith(extensions):
             files.append(file_object.path)
         elif file_object.is_dir() and depth > -1:
-            files.extend(find_files_with_extension(directory=file_object.path, depth=depth-1, extension=extension))
+            files.extend(find_files_with_extension(directory=file_object.path, depth=depth-1, extensions=extensions))
         else:
             continue
     
@@ -30,8 +30,8 @@ def find_files_with_extension(directory: str, depth: int = 0, extension: str = t
 
 
 if __name__ == "__main__":
-    directory: str =  r"C:\Users\marti\PythonProjects"
-    files: Tuple[str] = find_files_with_extension(directory=directory, depth=3, extension=(".txt", ".html", ".cpp"))
+    directory: str =  os.path.abspath(".")
+    files: Tuple[str] = find_files_with_extension(directory=directory, depth=3, extensions=(".txt", ".html", ".cpp"))
      
     print(files)
  
